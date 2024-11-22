6518f5a0084f2f10ae1f955e0d167d14eacda116
Satyamray21 <satyamray43@gmail.com>
2024-11-22 11:32:04 +0530

    JSquestion

---
+++ DataType/AllDataType.js
@@ -0,0 +1,5 @@
+// primitive 1.Number 2.Symbol 3.BigInt 4.undefined 5.null 6.String 7.boolean
+// non primitive 1.Array 2.Object 3.Function
+let number = 100;
+let isCondition = true;
+console.table([number,isCondition]);
