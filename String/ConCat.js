4ddeb42db839f415a795270f6d489bb83bb2c24b
Satyamray21 <satyamray43@gmail.com>
2024-11-23 11:16:54 +0530

    Concept till String completed

---
+++ String/ConCat.js
@@ -0,0 +1,11 @@
+let s1 = "Satyam"
+let s2 = "Ray"
+let s3 = s1 +" " +s2;
+//console.log(s3.length);
+let sentence = "I am Learning JavaScript";
+let word ="JavaScript";
+console.log(sentence.includes(word));
+let strings = "helloWord";
+console.log(strings.toUpperCase());
+let s = "Javascript";
+console.log(s.reverse);
