f38356f0090f19628fc40873e25b01fe9563f87d
Satyamray21 <satyamray43@gmail.com>
2024-11-23 17:35:04 +0530

    DateAndTime

---
+++ DateAndTime/DateAndTime.js
@@ -0,0 +1,13 @@
+current=new Date();
+console.log(current);
+console.log(current.getDate());
+console.log(current.getTime().toString());
+console.log(current.getHours());
+console.log(`${(current.getMonth())},${current.getFullYear()},${current.getDay()}`);
+const current1 = new Date("2024-11-20");
+const current2 = new Date("2024-11-23");
+
+const current3 = current2 - current1; 
+const differenceInDays = current3 / (1000 * 60 * 60 * 24); 
+
+console.log(`Difference in days: ${differenceInDays}`);
